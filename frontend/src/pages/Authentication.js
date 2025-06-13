749303442a8b3b23c5592dd1583b9f13fbc3368e
WodaMineralna <raknieborak65@gmail.com>
2025-06-13 20:55:47 +0200

    Implementing the Auth Action - user creation, user logging in

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,7 +1,56 @@
-import AuthForm from '../components/AuthForm';
+import { redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({ request, params }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login"; // 'login' is set as default, if "mode" is undefined
+
+  if (mode !== "login" && mode !== "signup") {
+    throw new Response(JSON.stringify({ message: "Unsupported mode." }), {
+      status: 422,
+    });
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  const url = `http://192.168.1.18:8080/${mode}`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    console.log(response); // DEBUGGING
+    return response;
+  }
+
+  if (!response.ok) {
+    throw new Response(
+      JSON.stringify({ message: "Could not authenticate user." }),
+      {
+        status: 500,
+      }
+    );
+  }
+
+  // TODO: manage the token
+
+  console.log(response)
+  console.log(await response.json()); // DEBUGGING
+
+  return redirect("/");
+}

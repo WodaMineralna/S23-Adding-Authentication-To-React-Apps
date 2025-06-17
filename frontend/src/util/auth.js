5c7b2ea87d2e612e7bacbffe512b449cf789e458
WodaMineralna <raknieborak65@gmail.com>
2025-06-17 21:23:27 +0200

    Adding Route Protection for specified pages

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,3 +1,5 @@
+import { redirect } from "react-router-dom";
+
 export default function getAuthToken() {
   const token = localStorage.getItem("token");
   return token;
@@ -5,4 +7,13 @@ export default function getAuthToken() {
 
 export function tokenLoader() {
   return getAuthToken();
+}
+
+export function checkAuthLoader() {
+  const token = getAuthToken();
+
+  if (!token) {
+    // or we can just throw an error
+    return redirect("/auth");
+  }
 }
